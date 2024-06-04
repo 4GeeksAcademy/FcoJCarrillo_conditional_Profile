aaef99b80e7222021b443919e91795d912807ea8
Francisco Javier Carrillo Carrillo <133151654+FcoJCarrillo@users.noreply.github.com>
2024-06-04 18:38:53 +0000

    Ejercicio terminado

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,4 @@
+import { right } from "@popperjs/core";
 import "../style/index.css";
 
 /**
@@ -29,18 +30,42 @@ function render(variables = {}) {
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
 
+  /*
+  FORMA 1
+  if (variables.name == null) variables.name = "First Name";
+  if (variables.lastName == null) variables.lastName = "Last Name";
+  if (variables.role == null) variables.role = "";
+  if (variables.city == null) variables.city = "";
+  if (variables.country == null) variables.country = "";*/
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+          <h1>${variables.name == null ? "First Name" : variables.name} ${
+    variables.lastName == null ? "Last Name" : variables.lastName
+  }</h1>
+          <h2>${variables.role == null ? "Select a Role" : variables.role}</h2>
+          <h3>${variables.city == null ? "Select a City" : variables.city}
+            ${
+              variables.country == null ? "Select a Country" : variables.country
+            }</h3>
+          <ul class="${
+            variables.socialMediaPosition == null
+              ? "position-right"
+              : variables.socialMediaPosition
+          }">
+            <li><a href="https://twitter.com/${
+              variables.twitter == null ? "" : variables.twitter
+            }"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="https://github.com/${
+              variables.github == null ? "" : variables.github
+            }"><i class="fab fa-github"></i></a></li>
+            <li><a href="https://linkedin.com/school/${
+              variables.linkedin == null ? "" : variables.linkedin
+            }"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="https://instagram.com/${
+              variables.instagram == null ? "" : variables.instagram
+            }"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
